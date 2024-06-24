2a5635a49b5d1c2907da4a7a33ed0625e01bf1f4
tanmaygoyal875 <tanmaygoyal875@gmail.com>
2024-06-24 06:44:30 +0530

    first commit

--- node_modules/@chakra-ui/hooks/dist/declarations/src/use-event-listener.d.ts
+++ node_modules/@chakra-ui/hooks/dist/declarations/src/use-event-listener.d.ts
@@ -5,11 +5,11 @@ export declare type EventListenerEnv = (() => DocumentOrElement) | DocumentOrEle
  *
  * @param event the event name
  * @param handler the event handler function to execute
- * @param doc the dom environment to execute against (defaults to `document`)
+ * @param env the dom environment to execute against (defaults to `document`)
  * @param options the event listener options
  *
  * @internal
  */
-export declare function useEventListener<K extends keyof DocumentEventMap>(event: K | (string & {}), handler: (event: DocumentEventMap[K]) => void, env?: EventListenerEnv, options?: boolean | AddEventListenerOptions): () => void;
+export declare function useEventListener<K extends keyof DocumentEventMap>(event: K | (string & {}), handler?: (event: DocumentEventMap[K]) => void, env?: EventListenerEnv, options?: boolean | AddEventListenerOptions): () => void;
 export {};
 //# sourceMappingURL=use-event-listener.d.ts.map
